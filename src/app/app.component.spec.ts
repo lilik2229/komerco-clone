8bfcf81486a71b4b5f0de0903e05f7771a4c24bd
ShunKOZAWA <crewfanq@gmail.com>
2018-07-20 22:46:01 +0900

    app.compのテストを設定

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
+import {
+    RouterTestingModule
+} from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,6 +11,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
   
