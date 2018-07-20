3035a5c9efbab05335593c0d4ee42b26cf235c91
ShunKOZAWA <crewfanq@gmail.com>
2018-07-20 17:16:08 +0900

    articles取得処理を追加

--- src/app/home/articles/articles.component.ts
+++ src/app/home/articles/articles.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ArticleService } from '../../shared/apis/article/article.service';
+
 import { Article } from '../../shared/models/article/article';
 
 @Component({
@@ -10,9 +12,36 @@ import { Article } from '../../shared/models/article/article';
 export class ArticlesComponent implements OnInit {
   articles:Article[] = new Array;
   
-  constructor() { }
+  constructor(
+    private articleService :ArticleService 
+  ) { }
 
   ngOnInit() {
+    const countOfArticles:number = 3;
+    this.setArticles(countOfArticles);
+  }
+
+  private setArticles(countOfArticles :number) :void{
+    this.articleService
+      .getArticles(countOfArticles)
+      .then(
+        (querySnapshot)=>{
+          querySnapshot.forEach((doc)=> {
+            const article:Article =
+              new Article(
+                doc.data().description,
+                doc.data().imageSrc,
+                doc.data().link
+              );
+            this.articles.push(article);
+          });
+        }
+      )
+      .catch(
+        (error)=>{
+          console.log(error);
+        }
+      );
   }
 
 }
