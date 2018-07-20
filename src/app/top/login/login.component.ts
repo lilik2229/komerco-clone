80b6e0bffa4627547fba69db1db702388231dc58
ShunKOZAWA <crewfanq@gmail.com>
2018-07-20 17:15:51 +0900

    authのパス変更対応

--- src/app/top/login/login.component.ts
+++ src/app/top/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from '../../shared/auth/auth.service';
+import { AuthService } from '../../shared/apis/auth/auth.service';
 
 @Component({
   selector: 'app-login',
